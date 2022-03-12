2cc6bbd7831ce9f11919bcc861befcf79531dcd5
Andrew Rohl <andrew.rohl@curtin.edu.au>
2022-03-12 17:26:30 +0800

    Update cricket.py
    Added requests to dependencies

--- cricket.py
+++ cricket.py
@@ -5,7 +5,7 @@
 # <bitbar.author>Andrew Rohl</bitbar.author>
 # <bitbar.author.github>arohl</bitbar.author.github>
 # <bitbar.desc>Provides current score from cricinfo</bitbar.desc>
-# <bitbar.dependencies>python3</bitbar.dependencies>
+# <bitbar.dependencies>python3 requests</bitbar.dependencies>
 
 import requests
 import datetime
