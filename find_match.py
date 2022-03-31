2e20cb2c32a5c0a204026971f1fc0b38e7a62e9b
Andrew Rohl <andrew.rohl@curtin.edu.au>
2022-03-31 22:09:00 +0800

    Reformatted by black

--- find_match.py
+++ find_match.py
@@ -26,20 +26,26 @@ page = requests.get(matches_url)
 matches_json = page.json()
 
 found = False
-for match in matches_json['matches']:
-    for team in match['teams']:
-        if team['team']['abbreviation'] == my_team:
+for match in matches_json["matches"]:
+    for team in match["teams"]:
+        if team["team"]["abbreviation"] == my_team:
             found = True
-            start_time = match['startTime']
-            home_team = match['teams'][0]['team']['name']
-            away_team = match['teams'][1]['team']['name']
-            match_id = match['objectId']
-            series_id = match['series']['objectId']
+            start_time = match["startTime"]
+            home_team = match["teams"][0]["team"]["name"]
+            away_team = match["teams"][1]["team"]["name"]
+            match_id = match["objectId"]
+            series_id = match["series"]["objectId"]
 
 if found:
-    config = {'seriesId': series_id, 'matchId': match_id, 'homeTeam': home_team, 'awayTeam': away_team, 'startTime': start_time}
+    config = {
+        "seriesId": series_id,
+        "matchId": match_id,
+        "homeTeam": home_team,
+        "awayTeam": away_team,
+        "startTime": start_time,
+    }
 else:
     exit()
 
-with open(config_with_path, 'w') as f:
+with open(config_with_path, "w") as f:
     json.dump(config, f)
